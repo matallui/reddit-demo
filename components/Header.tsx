d2cb6c69a90483e7d9c3cf65c99823d15dbf6a0b
Luis Taniça <ltanica@mersive.com>
2022-06-02 15:59:10 -0600

    fix header for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,7 +25,7 @@ const Header: React.FC<Props> = () => {
 
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm items-center">
-      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer mr-2">
         <Link href="/">
           <Image
             src="https://links.papareact.com/fqy"
@@ -35,7 +35,7 @@ const Header: React.FC<Props> = () => {
         </Link>
       </div>
 
-      <div className="flex items-center mx-7 xl:min-w-[300px]">
+      <div className="hidden sm:flex items-center mx-5 xl:min-w-[300px]">
         <HomeIcon className="h-5 w-5" />
         <p className="flex-1 ml-2 hidden lg:inline">Home</p>
         <ChevronDownIcon className="h-5 w-5" />
